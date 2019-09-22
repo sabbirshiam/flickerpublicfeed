2be2d529e85062d565291fa3338e070670ee2389
sabbirshiam <sabbirshiam@gmail.com>
2019-09-22 14:54:19 +0900

    initial project setup files.

---
+++ app/src/main/java/com/example/flickerimagegallery/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.flickerimagegallery.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flickerimagegallery.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
