0845dfab225024cf935049595f24e2f324e3606e
sabbirshiam <sabbirshiam@gmail.com>
2020-02-09 01:01:36 +0900

    sending file to server using multipart.

--- app/src/main/java/com/example/flickerimagegallery/data/repositories/file_upload/FileUploadRepository.kt
+++ app/src/main/java/com/example/flickerimagegallery/data/repositories/file_upload/FileUploadRepository.kt
@@ -1,12 +1,12 @@
 package com.example.flickerimagegallery.data.repositories.file_upload
 
 import android.content.Context
+import android.util.Log
 import com.example.flickerimagegallery.GalleryApplication
 import com.example.flickerimagegallery.data.entities.FileUploadResponse
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import okhttp3.ResponseBody
 import retrofit2.Response
 import java.io.File
 
@@ -15,14 +15,35 @@ class FileUploadRepository(private val context: Context) {
 
     suspend fun uploadFile(path: String): Response<FileUploadResponse> {
         val file = File(path)
+        Log.e("File", "Files exists..")
+        if (file.exists()) {
+            val requestFile: RequestBody = RequestBody.create(
+                MediaType.parse("image/*"), // any image can be uploaded.
+                file
+            )
 
-        val requestFile: RequestBody = RequestBody.create(
-            MediaType.parse(file.absolutePath),
-            file
-        )
+            //context.contentResolver.getType(file.toUri())
 
-        // MultipartBody.Part is used to send also the actual file name
-        val body : MultipartBody.Part = MultipartBody.Part.createFormData("picture", file.name, requestFile)
-        return application.getRetrofitIntance().fileUploadService.uploadFile(requestFile)
+            // if we want to create a builder
+            val builder = MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart(
+                    "avatar",
+                    file.name,
+                    RequestBody.create(MediaType.parse("image/*"), file)
+                )
+
+            // MultipartBody.Part is used to send also the actual file name
+            val body: MultipartBody.Part =
+                MultipartBody.Part.createFormData("avatar", file.name, requestFile)
+            return application.getRetrofitIntance().fileUploadService.uploadFile(body)
+        }
+
+        val builder = MultipartBody.Builder().setType(MultipartBody.FORM)
+            .addFormDataPart("avatar", file.name, RequestBody.create(MultipartBody.FORM, file))
+        val body: MultipartBody.Part =
+            MultipartBody.Part.createFormData("avatar", file.name, null)
+        return application.getRetrofitIntance().fileUploadService.uploadFile(body)
+
+        //return Response.error(404, ResponseBody.create(MediaType.get(""), ""))
     }
 }
--- app/src/main/java/com/example/flickerimagegallery/data/repositories/file_upload/FileUploadService.kt
+++ app/src/main/java/com/example/flickerimagegallery/data/repositories/file_upload/FileUploadService.kt
@@ -3,7 +3,6 @@ package com.example.flickerimagegallery.data.repositories.file_upload
 import com.example.flickerimagegallery.BuildConfig
 import com.example.flickerimagegallery.data.entities.FileUploadResponse
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -12,6 +11,6 @@ interface FileUploadService {
     @Multipart
     @POST(BuildConfig.FILE_UPLOAD_API_PREFIX.plus("upload-avatar"))
     suspend fun uploadFile(
-        @Part("avatar") file: RequestBody
+        @Part file: MultipartBody.Part
     ): retrofit2.Response<FileUploadResponse>
 }
